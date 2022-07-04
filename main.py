54b1a58106acc0bd7db288f4f1ffe26d0e18a409
Jordan-Fielding <107819004+Jordan-Fielding@users.noreply.github.com>
2022-07-04 16:18:58 +1000

    test

--- main.py
+++ main.py
@@ -9,25 +9,23 @@ GPIO.setup(22, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(27, GPIO.OUT)         #LED output pin
 
 i=GPIO.input(22)
-try:
-    time.sleep(2) # to stabilize sensor
-    while True:
-        ts = time.time()
-        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
-        if i==0:                 #When output from motion sensor is LOW
-            print ("No bugs detected",i)
-            GPIO.output(3, 0)  #Turn OFF LED
-            time.sleep(0.1)
-        if i==1:               #When output from motion sensor is HIGH
-            print ("bugs detected",i)
-            GPIO.output(3, 1)  #Turn ON LED
-            camera.capture('image_Time_{}.jpg'.format(st))
-            os.system('libcamera-jpeg -o /home/pi/Pictures/image_Time_{}.jpg'.format(st))
-            camera.close()  #Capture an Image
-            time.sleep(0.1)
 
-except:
-    GPIO.cleanup()
+time.sleep(2) # to stabilize sensor
+while True:
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+    if i==0:                 #When output from motion sensor is LOW
+        print ("No bugs detected",i)
+        GPIO.output(3, 0)  #Turn OFF LED
+        time.sleep(0.1)
+    if i==1:               #When output from motion sensor is HIGH
+        print ("bugs detected",i)
+        GPIO.output(3, 1)  #Turn ON LED
+        camera.capture('image_Time_{}.jpg'.format(st))
+        os.system('libcamera-jpeg -o /home/pi/Pictures/image_Time_{}.jpg'.format(st))
+        camera.close()  #Capture an Image
+        time.sleep(0.1)
+
 
 
 
