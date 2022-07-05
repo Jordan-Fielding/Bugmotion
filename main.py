2f5dea9edf4ea1aa81d357ee1e93e4d068e83a82
Jordan-Fielding <107819004+Jordan-Fielding@users.noreply.github.com>
2022-07-05 15:52:18 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import RPi.GPIO as GPIO                 #Used to Import the LED
 import time                             #Used to allow wait times
 from picamera import PiCamera           #Used to import the Camera
 from gpiozero import MotionSensor       #Used to import the MotionSensor
+import os
 
 pir = MotionSensor(4)
 camera = PiCamera()                     #Camera Initialization
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)                  #Setting the GPIO Mode
-GPIO.setup(22, GPIO.OUT)                #LED output pin
+GPIO.setup(22, GPIO.OUT)
+
+cwd = os.getcwd()
+
+#LED output pin
 
 
 #To stabilize sensor and Camera
@@ -23,7 +28,9 @@ while True:
     GPIO.output(22, 1)
 
     #Sets Img path and filename, Saves to USB Connected
-    file_name = "/media/Capture_" + str(time.time()) + ".jpg"
+    print("File will be Saved in: " + cwd)
+    file_name = cwd + str(time.time()) + ".jpg"
+
 
     #Used to wait for 0.1 Seconds for Camera to be ready
     time.sleep(0.1)
